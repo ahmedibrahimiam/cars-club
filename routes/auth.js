845c5caf6519414ab16ce9419f61b1a259f0db13
[ahmedibrahimiam] <[ahmedibrahimiam@gmail.com]>
2025-11-20 17:30:03 +0200

    Final Fix: Match name field in auth.js with User.js Schema

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    const { username, email, password } = req.body;
+    const { name, email, password } = req.body;
 
     const exists = await User.findOne({ email });
     if (exists) return res.json({ success: false, message: "Email already exists" });
@@ -14,7 +14,7 @@ router.post("/register", async (req, res) => {
     const hashed = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-        username,
+        name,
         email,
         password: hashed
     });
