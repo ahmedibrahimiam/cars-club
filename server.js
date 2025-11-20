bd685f5cf9c0c1163bd58667782da4d1d9bc095c
[ahmedibrahimiam] <[ahmedibrahimiam@gmail.com]>
2025-11-20 16:34:11 +0200

    Final fix: Disable dotenv for Vercel

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
 
-dotenv.config();
+//dotenv.config();
 
 const app = express();
 
