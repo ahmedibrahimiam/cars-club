f4a62e010c4bcaff68d5deef108428a1b0f9df19
[ahmedibrahimiam] <[ahmedibrahimiam@gmail.com]>
2025-11-22 21:23:18 +0200

    fix: corrected port config

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/cars", carsRoutes);
 app.use("/api/parts", partsRoutes); // ⬅️ تأكد من وجوده
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
